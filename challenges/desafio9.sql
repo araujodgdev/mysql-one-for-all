46c1a56188da0a68c0489d890d2749bfb776203e
Douglas Araújo <devdoug45@gmail.com>
2023-07-27 17:53:22 -0300

    feat

---
+++ challenges/desafio9.sql
@@ -0,0 +1,11 @@
+SELECT 
+	COUNT(hstrc.pessoa_usuaria) AS musicas_no_historico
+FROM
+	SpotifyClone.historico_reproducao hstrc
+JOIN
+	SpotifyClone.pessoa_usuaria pu
+ON
+	hstrc.pessoa_usuaria = pu.pessoa_usuaria_id
+WHERE
+	pu.nome_pessoa_usuaria = "Barbara Liskov"
+;
